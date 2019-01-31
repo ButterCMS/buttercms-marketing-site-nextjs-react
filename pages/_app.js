7ca79feadd672a7f3972c7f2af63f1556536a1ca
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-02-01 00:11:14 +0530

    feat(app): Adds basic setup for Next.js

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import App, { Container } from "next/app";
+
+class Layout extends Component {
+  render() {
+    const { children } = this.props;
+    return <div className="layout">{children}</div>;
+  }
+}
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Container>
+    );
+  }
+}
