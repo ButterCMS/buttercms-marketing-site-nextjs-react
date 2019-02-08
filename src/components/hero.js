6ab7a61bf4a9212697e8aa1fbe9d3ab88053b5ee
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-02-09 00:14:59 +0530

    feat(app): Shows data for hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import withButter from "../lib/with-butter-cms";
 
-class Hero extends Component {
-  componentDidMount() {
-    this.props.butter.page
-      .retrieve("*", "index")
-      .then(function(resp) {
-        console.log(resp.data);
-      })
-      .catch(function(resp) {
-        console.log(resp);
-      });
-  }
+function Hero(props) {
+  const [fields, setFields] = useState({ fields: [] });
+  const [error, setError] = useState({ data: {} });
+  const fetchData = async () => {
+    try {
+      const response = await props.butter.page.retrieve("*", "hero");
 
-  render() {
-    return (
-      <section className="hero">
-        <div className="hero-left-decoration is-revealing" />
-        <div className="hero-right-decoration is-revealing" />
-        <div className="container">
-          <div className="hero-inner">
-            <div className="hero-copy">
-              <h1 className="hero-title mt-0 is-revealing">
-                Landing template for startups
-              </h1>
-              <p className="hero-paragraph is-revealing">
-                Our landing page template works on all devices, so you only have
-                to set it up once, and get beautiful results forever.
-              </p>
-              <p className="hero-cta mb-0 is-revealing">
-                <a className="button button-primary button-shadow" href="#">
-                  Get started free
-                </a>
-              </p>
-            </div>
-            <div className="hero-illustration" />
+      setFields(response.data.data.fields);
+    } catch (error) {
+      setError(error.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <section className="hero">
+      <div className="hero-left-decoration is-revealing" />
+      <div className="hero-right-decoration is-revealing" />
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-copy">
+            <h1 className="hero-title mt-0 is-revealing">{fields.title}</h1>
+            <p className="hero-paragraph is-revealing">{fields.sub_title}</p>
+            <p className="hero-cta mb-0 is-revealing">
+              <a className="button button-primary button-shadow" href="#">
+                {fields.button_text}
+              </a>
+            </p>
           </div>
+          <div className="hero-illustration" />
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 Hero.propTypes = {
