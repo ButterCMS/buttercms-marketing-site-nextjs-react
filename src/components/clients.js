735e5212da55bfa1ef6b5aa9bd6703dadf97b3d9
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-02-09 00:15:09 +0530

    feat(app): Shows data for clients section

--- src/components/clients.js
+++ src/components/clients.js
@@ -1,41 +1,46 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import withButter from "../lib/with-butter-cms";
 
-import MicrosoftLogo from "../assets/images/microsoft-logo.svg";
-import HubspotLogo from "../assets/images/hubspot-logo.svg";
-import TinderLogo from "../assets/images/tinder-logo.svg";
-import AirbnbLogo from "../assets/images/airbnb-logo.svg";
-import FacebookLogo from "../assets/images/facebook-logo.svg";
+function Clients(props) {
+  const [fields, setFields] = useState({ fields: [] });
+  const [error, setError] = useState({ data: {} });
+  const fetchData = async () => {
+    try {
+      const response = await props.butter.page.retrieve("*", "clients");
 
-class Clients extends Component {
-  render() {
-    return (
-      <section className="clients section">
-        <div className="container">
-          <div className="clients-inner section-inner has-top-divider">
-            <div className="container-sm">
-              <ul className="list-reset mb-0">
-                <li className="is-revealing">
-                  <img src={FacebookLogo} alt="Facebook logo" />
-                </li>
-                <li className="is-revealing">
-                  <img src={TinderLogo} alt="Tinder logo" />
-                </li>
-                <li className="is-revealing">
-                  <img src={AirbnbLogo} alt="Airbnb logo" />
-                </li>
-                <li className="is-revealing">
-                  <img src={MicrosoftLogo} alt="Microsoft logo" />
-                </li>
-                <li className="is-revealing">
-                  <img src={HubspotLogo} alt="Hubspot logo" />
-                </li>
-              </ul>
-            </div>
+      setFields(response.data.data.fields);
+    } catch (error) {
+      setError(error.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <section className="clients section">
+      <div className="container">
+        <div className="clients-inner section-inner has-top-divider">
+          <div className="container-sm">
+            <ul className="list-reset mb-0">
+              <li className="is-revealing">
+                <img src={fields.client_1} alt="Client logo" />
+              </li>
+              <li className="is-revealing">
+                <img src={fields.client_2} alt="Client logo" />
+              </li>
+            </ul>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
-export default Clients;
+Clients.propTypes = {
+  butter: PropTypes.object.isRequired
+};
+
+export default withButter(Clients);
