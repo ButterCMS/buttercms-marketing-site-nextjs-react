3ee774fffdff4c3480ad73612997110405d5a017
Orly Knop <orly.knop@gmail.com>
2020-12-31 19:28:35 +0200

    refactor: moved animation from main to question component

--- components/question.js
+++ components/question.js
@@ -1,13 +1,21 @@
+import React from "react";
+
 export default function Question({ question, answer }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+
+  function toggleQuestion() {
+    setCollapsed(!collapsed);
+  }
   return (
-    <>
+    <div className={!collapsed ? "is-open" : ""}>
       <div className="accordion-title">
         <span>{question}</span>
-        <div className="accordion-icon" />
+        <div className="accordion-icon" onClick={toggleQuestion} />
       </div>
+
       <div className="accordion-body">
         <p>{answer}</p>
       </div>
-    </>
+    </div>
   );
 }
